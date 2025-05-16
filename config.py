f95919d055c7beea3e0aa86f95789f1e2fc90924
mmtq <mirtarhimul@gmail.com>
2025-05-16 21:39:36 +0600

    init, tokenizer, casual self attention

---
+++ config.py
@@ -0,0 +1,7 @@
+class GPT2Config:
+    vocab_size = 50257  # GPT-2 vocabulary size
+    max_position_embeddings = 512  # Maximum sequence length
+    n_layer = 6  # Number of transformer blocks
+    n_head = 8  # Number of attention heads
+    n_embd = 512  # Embedding size
+    dropout = 0.1  # Dropout rate
---
+++ model/attention.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class CasualSelfAttention(nn.Module):
+    def __init__(self, config):
+        super.__init__()
+        # assert config.n_embd % config.n_head == 0, "n_embd must be divisible by n_head"
+        
+        self.head_dim = config.n_embd // config.n_head
+        self.n_head = config.n_head
+        
+        # Linear layers for query, key, and value projections
+        self.query = nn.Linear(config.n_embd, config.n_embd, bias=True)
+        self.key = nn.Linear(config.n_embd, config.n_embd, bias=True)
+        self.value = nn.Linear(config.n_embd, config.n_embd, bias=True)
+        
+        # Output Projection
+        
+        self.proj = nn.Linear(config.n_embd, config.n_embd, bias=True)
+        
+        self.dropout = nn.Dropout(config.dropout)
+    
+    def forward(self, x, mask=None):
+        B,T,C = x.size()
+        
+        # Comput Q,K,V matrices
+        Q = self.query(x).view(B, T, self.n_head, self.head_dim).transpose(1,2)
+        K = self.key(x).view(B, T, self.n_head, self.head_dim).transpose(1,2)
+        V = self.value(x).view(B, T, self.n_head, self.head_dim).transpose(1,2)
+        
+        # Compute attention scores
+        attn_scores = (Q @ K.transpose(-2, -1)) / (self.head_dim ** 0.5)
+        
+        if mask is not None:
+            attn_scores = attn_scores.masked_fill(mask==0, float('-inf'))
+            
+        attn_weights = torch.softmax(attn_scores, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+        
+        attn_output = (attn_weights @ V).transpose(1,2).contiguous.view(B, T, C)
+        
+        return self.proj(attn_output)
+        
